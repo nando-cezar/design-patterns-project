e3cf0bcb5dcfff8c679fc1527604e39ceced4c91
Luis Fernando Cezar Dos Santos <nandocezar22@gmail.com>
2023-05-18 14:35:47 -0300

    finish

--- nutrient/src/main/java/org/example/singleton/Catalogo.java
+++ nutrient/src/main/java/org/example/singleton/Catalogo.java
@@ -7,21 +7,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Catalogo implements ArmazemNutrientes{
-
-    private final Map<String, Nutriente> nutrientes = new HashMap<>();
+    private Map<String, Nutriente> nutrientes = new HashMap<>();
     @Override
     public Nutriente create(String nome, Nutriente nutriente) {
-        var data = nutrientes.get(nome);
+        var data = this.nutrientes.get(nome);
 
         if(data == null){
-            nutrientes.put(nome, nutriente);
+            this.nutrientes.put(nome, nutriente);
+            return this.nutrientes.get(nome);
         }else{
             data.setNome(nutriente.getNome());
             data.setUnidade(nutriente.getUnidade());
             data.setCaloriaPorUnidade(nutriente.getCaloriaPorUnidade());
-            nutrientes.put(nome, data);
+            this.nutrientes.put(nome, data);
+            return this.nutrientes.get(nome);
         }
-        return data;
     }
 
     @Override
